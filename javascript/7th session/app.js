72cbc7f462e041f32fe69500867be37effd0e36b
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-04-05 10:05:33 +0530

    make project

--- javascript/7th session/app.js
+++ javascript/7th session/app.js
@@ -1,2 +1,11 @@
-console.log((5>=17)&&(99==99));
-console.log(7^8);
+// console.log((5>=17)&&(99==99));
+// console.log(7^8);
+let age=17;
+if(age>=18){
+    console.log("eligible for vote");
+    
+}
+else{
+    console.log("not eligible for vote");
+    
+}
