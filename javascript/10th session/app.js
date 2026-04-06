1fe978b13d2eb9641658ce246cd2591ad1b9e01e
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-04-06 21:21:58 +0530

    practice

---
+++ javascript/10th session/app.js
@@ -0,0 +1,78 @@
+// let num=10;
+
+// if(num>=0){
+//     if(num==0){
+//         console.log(num,"is zero");
+        
+//     }
+//     else
+//     console.log(num,"is +ve");{  
+//     }
+// }
+// else{
+//     console.log(num,"is -ve");
+    
+// }
+// let num=12;
+// if(num>0){
+//     console.log(num,"is +ve");
+    
+// }
+// else{
+//     if(num==0){
+//         console.log(num,"is 0");
+        
+//     }
+//     else{
+//         console.log(num,"is -ve");
+        
+//     }
+// }
+// let CP=10,SP=20;
+// if(SP>=CP){
+//     if(SP==CP){
+//         console.log("no profit no loss"); 
+//     }
+//     else{
+//     console.log("Profit");
+//     } 
+// }
+// else{
+//     console.log("loss");
+    
+// // }
+// let num=-11;
+// if(num>=0){
+// if(num==0){
+// console.log(num,"is zero");
+// }else{
+// console.log(num,"is positive");
+// }
+// }
+// else{
+// console.log(num,"is negative");
+// }
+// let score=85,attendance=85;
+// if (score > 90) {
+//   if (attendance > 80) {
+//     console.log('Passed with distinction');
+//   } else {
+//    console.log('Passed');
+//     ;
+//   }
+// } else {
+//   console.log('Failed');
+   
+// }
+let num=1;
+if(num%2==0 || num%3==0){
+    if(num%2!=0 && num%3==0){
+        console.log("divisible by once")
+    }
+    else{
+    console.log("divisible by both");
+}
+}
+else{
+    console.log("not divisible by both");
+}
