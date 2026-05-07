5fd3881c55359f360dcf89d8a6e7f465bb4539ae
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-05-07 08:24:36 +0530

    practice

--- javascript/array/app.js
+++ javascript/array/app.js
@@ -9,14 +9,25 @@
 // for(let i=arr.length-1;i>=0;i--){
 //     console.log(arr[i]);
 // }
- let arr=[1,2,3,4,5];
- let i=0;
- while(i<arr.length){
-    console.log(arr[i])
-    i++;
+//  let arr=[1,2,3,4,5];
+//  let i=0;
+//  while(i<arr.length){
+//     console.log(arr[i])
+//     i++;
+//  }
+ 
+ let arr=[[1,2,3],[4,5,6],[7,8,9]];
+ for(let i=0;i<arr.length;i++){
+   for(let j=0;j<arr.length;j++){
+      process.stdout.write(arr[i][j]+" ");
+   }
+   console.log();
  }
- 
- 
+
+
+
+
+
 
 
  
