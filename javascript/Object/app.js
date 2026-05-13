f0aed32f1925c2ad403dc524126d443a7fbf4a65
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-05-13 10:00:39 +0530

    practice

--- javascript/Object/app.js
+++ javascript/Object/app.js
@@ -6,6 +6,7 @@
 // }
 // };
 // student.print();
+
 // let person={
 //     name2: "Amar",
 //     address:{
@@ -56,14 +57,15 @@
 // }
 // delete employee.salary;
 // console.log(employee);
+
 // let user={
 //     name1: "Abhi",
 //     greet:function(){
 //         console.log("hello!",this.name1);
-        
 //     }
 // }
 // user.greet();
+
 // let person={
 //     name:"Rahul",
 //     address: {
@@ -81,12 +83,74 @@
 // }
 // console.log(user[key]);
 
-let person={
-    name: "Abhi",
-    age: 22,
-    city:"Delhi"
-}
-console.log(person.keys(person));
+// let person = {
+//     name: "Abhi",
+//     age: 22,
+//     city: "Delhi"
+// };
+// // Get all keys of the object
+// let keys = Object.keys(person);
+// // Print each key
+// keys.forEach(function(key) {
+//     console.log(key);
+// });
+
+// let student={
+//     "Student name": "Ashwani kumar",
+//     id:101
+// }
+// console.log(student["Student name"]);
+
+// let obj=new Object();
+// obj.name1="Ashwani kumar",
+// obj.id=101
+// console.log(obj);
+
+// creating using constructor
+// function Student(StudentName,StudentId){
+//     this.name=StudentName;
+//     this.id=StudentId;
+//     this.display=function(){
+//         console.log(`Name=${this.name} id=${this.id}`);
+//     }
+// }
+// let s1= new Student("Ravi",101);
+// console.log(s1);
+// console.log("Type=",typeof(s1));
+// s1.display();
+
+//destructing String 
+// let obj={name: "Anuj raj",id:101};
+// console.log(obj);
+// let{name,id}=obj;
+// console.log(`name=${name} id=${id}`);
+
+// accesing keys
+// let obj={name: "Anuj raj",id:101};
+// console.log(obj);
+// let keys=Object.keys(obj);
+// console.log(keys);
+// for(key of keys){
+//     console.log(key," : ",obj[key]);
+    
+// }
+
+// entries
+// let obj={name: "Anuj raj",id:101};
+// console.log(obj);
+// let arr=Object.entries(obj);
+// console.log(arr);
+// for([Key,value]of arr){
+//     console.log(`key=${key} id=${value}`);
+    
+// }
+
+
+
+
+
+
+
 
 
 
--- javascript/Strings/app.js
+++ javascript/Strings/app.js
@@ -1,22 +1,71 @@
-let name1;
-name1=(prompt("Enter array elements: "))
-let vowelCount=0;
-let ConCount=0;
-for(let i=0;i<name1.length;i++){
-  if(name1=="a"|| name1=="e"||name1=="i"|| name1=="o"|| name1=="u"){
-    vowelCount++;
-  }
-  else{
-    ConCount++;
-  }
-}
-console.log("total vowel count: ",vowelCount);
-console.log("total conCount: ",ConCount);
+//reverse string
 
-// console.log(name1);
+// let str="Coding Age";
+// for(let i=str.length-1;i>=0;i--){
+//   console.log(str[i]);
+  
+// }
+// console.log(str.length);
 
-// let id=111;
-// console.log("name:",name1,"id:",id);
-// console.log(`Name: ${name1} id: ${id}`);
-// console.log(name1[9]);
-// 
+//count string
+// let str="Hello World";
+// let count=0;
+// for(let i=0;i<str.length;i++){
+// count++
+// }
+// console.log(count);
+
+// count vowels and consonants
+
+// let name1="Coding age";
+// let vowelCount=0;
+// let ConCount=0;
+// for(let i=0;i<name1.length;i++){
+//   let ch=name1[i];
+//   if(ch=="a"|| ch=="e"||ch=="i"|| ch=="o"|| ch=="u"||ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U"){
+//     vowelCount++;
+//   }
+//   else{
+//     if (ch!=" ") {
+//             ConCount++;
+//         }
+//   }
+// }
+// console.log("total vowel count: ",vowelCount);
+// console.log("total conCount: ",ConCount);
+
+//  find duplicate characters
+// let str="Programming";
+// let count=0;
+// for(let i=0;i<str.length;i++){
+//   for(let j=i+1;j<str.length;j++){
+//     if(str[i]===str[j]){
+//       count++
+//        console.log(str[i]);
+//   break;
+// }
+//   }
+// }
+// console.log("Total duplicate characters:", count);
+
+// merge strings using concat
+// let str1="Coding";
+// let str2="";
+
+// let result=str1.concat(" ",str2["Age"]);
+// console.log(result);
+
+// copy one string into another
+// let str1="Coding";
+// let str2="";
+// str2 = str1.slice();
+// console.log("After Copying :", str2);
+ 
+// reverse using .reverse
+// let str="JavaScript";
+
+// let reverse = str.split("").reverse().join("");
+// console.log(reverse);
+
+
+
