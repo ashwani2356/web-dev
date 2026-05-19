56ef1e7641802f6b1c45a10fe7a5f01147583fd6
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-05-19 15:31:54 +0530

    changes

---
+++ javascript/1var,let,const/app.js
@@ -0,0 +1,16 @@
+// let num=100;
+// console.log(num);
+//  num=num+10;
+//  console.log(num);
+ 
+// const num=100;
+// console.log(num);
+// num=200;
+// console.log(num);
+
+// console.log(a);
+// var a=100;
+
+
+ 
+ 
---
+++ javascript/2dataTypeConversion/app.js
@@ -0,0 +1,21 @@
+// implicit conversion
+// let result=5+"2";
+// let sub=5-"2";
+// console.log(result);
+// console.log(sub);
+
+// explicit conversion
+ let str=120;
+//  let num=Number(str);
+//  let num1=parseFloat(str);
+ let num2=parseInt(str)
+//  console.log(num);
+//  console.log(typeof(num));
+//  console.log(typeof(num1));
+// console.log(typeof(num2));
+
+// console.log(Boolean(" "));
+
+// makes alert using alert key
+// let name=prompt("enter your name: ");
+//  alert(`Hello,${name},how are you?`);
