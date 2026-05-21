53bc45749085fc11f7323ada114d579094adae88
ashwani06 <rajeshchoudhary2356@gmail.com>
2026-05-21 11:18:55 +0530

    changes

--- javascript/2dataTypeConversion/app.js
+++ javascript/2dataTypeConversion/app.js
@@ -5,10 +5,10 @@
 // console.log(sub);
 
 // explicit conversion
- let str=120;
+//  let str=120;
 //  let num=Number(str);
 //  let num1=parseFloat(str);
- let num2=parseInt(str)
+//  let num2=parseInt(str)
 //  console.log(num);
 //  console.log(typeof(num));
 //  console.log(typeof(num1));
@@ -19,3 +19,15 @@
 // makes alert using alert key
 // let name=prompt("enter your name: ");
 //  alert(`Hello,${name},how are you?`);
+
+// take two user input from the user print their sum
+// let a,b,sum;
+// a=Number(prompt("enter a number"));
+// b=Number(prompt("enter a number"));
+// sum=a+b;
+// console.log(sum);
+
+let a=2,square;
+square=a**3;
+console.log(square);
+
